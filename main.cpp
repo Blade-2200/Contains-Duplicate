a2794023a70fc7efc71978667ac913319b1d2f83
Blade-2200 <daniel.lee.sedu@gmail.com>
2025-11-01 22:29:38 -0700

    Update Solution with Hash Table
    Faster solution allowing better performance with larger datasets.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
-// Solution Currently Takes too long
-
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> seen;
+
         for (int i = 0; nums.size() > i; i++) {
-            for (int j = i + 1; nums.size() > j; j++) {
-                if (nums[i] == nums[j]) {
-                    return true;
-                }
+            int target = nums[i];
+
+            if (seen.find(target) != seen.end()) {
+                return true;
             }
+            seen[nums[i]] = i;
         }
-        return false;
+        return {};
     }
 };
